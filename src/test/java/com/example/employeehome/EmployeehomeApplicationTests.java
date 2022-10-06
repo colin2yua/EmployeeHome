e3498c6c2e55b1b8557c99365fd70196f46f9f64
刘航 <>
2022-10-06 20:24:16 +0800

    '初始化项目'

---
+++ src/test/java/com/example/employeehome/EmployeehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeehomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
